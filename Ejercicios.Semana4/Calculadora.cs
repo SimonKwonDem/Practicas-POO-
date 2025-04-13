ceca6828a44014efad01f9dc79848eb4e0ff915e
Simon Kwon <simonkwon03@gmail.com>
2025-04-13 14:24:34 -0300

    class calculadora

---
+++ Ejercicios.Semana4/Calculadora.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios.Semana4
+{
+    internal class Calculadora
+    {
+        /*Crear una clase Calculadora.
+
+        Crear un método sumar(int a, int b) que devuelva una suma.
+        Crear un método sumar(double a, double b, double c) para sumar tres números.
+        Crear un método sumar(string mensaje, int a, int b) que imprima el mensaje por consola y luego la suma.
+        Crear un método que devuelva true si un número es par o false si es impar bool esPar(int a).    
+         */
+
+        public int Sumar(int a, int b)
+        {
+            return a + b;
+        }
+        public double SumarDecimal(double a, double b, double c)
+        {
+            return a + b + c;
+        }
+        public void MensajeSuma(string mensaje, int a, int b)
+        {
+            Console.WriteLine($"{mensaje}, {Sumar(a, b)}");
+        }
+        public bool ParImpar(int a)
+        {
+            if(a % 2 == 0)
+            {
+                return true;
+            }else return false; 
+        }
+    }
+}
--- Ejercicios.Semana4/Program.cs
+++ Ejercicios.Semana4/Program.cs
@@ -2,7 +2,7 @@
 
 internal class Program
 {
-     static void Main(string[] args)
+    static void Main(string[] args)
     {
         Persona persona1 = new Persona();
         persona1.MostrarDatos();
@@ -11,8 +11,8 @@ internal class Program
         Persona persona2 = new Persona("Simon");
         persona2.MostrarDatos();
         Console.WriteLine();
-        
-        Persona persona3 = new Persona("Juan", 45122333,21);
+
+        Persona persona3 = new Persona("Juan", 45122333, 21);
         persona3.MostrarDatos();
         Console.WriteLine();
 
@@ -23,5 +23,23 @@ internal class Program
         //Modifico la edad a través del método set.
         persona3.SetEdad(24);
         persona3.MostrarDatos();
+
+        Calculadora calculadora1 = new Calculadora();
+        Console.WriteLine();
+        
+        Console.WriteLine($"{calculadora1.Sumar(23, 12)}");
+        Console.WriteLine();
+
+        Console.WriteLine($"{calculadora1.SumarDecimal(24.5, 22.32, 11.2)}");
+        Console.WriteLine();
+
+        calculadora1.MensajeSuma("Resultado de la suma:", 23, 23);
+        Console.WriteLine();
+        
+        Console.WriteLine($"{calculadora1.ParImpar(10)}");
+        Console.WriteLine();
+
+        Console.WriteLine($"{calculadora1.ParImpar(7)}");
     }
+
 }
