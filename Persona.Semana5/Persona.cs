0471064d36e414f963933bad04c3d7a03268abae
Simon Kwon <simonkwon03@gmail.com>
2025-04-20 17:32:56 -0300

    Ejercicio Persona

---
+++ Persona.Semana5/Persona.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona.Semana5
+{
+    internal class Person
+    {
+        private int id;
+        private string nombre;
+        private int edad;
+
+        public int Id { get { return id; } set { id = value; } }
+        public string Nombre { get { return nombre; } set { nombre = value; } }
+        public int Edad { get { return edad; } set { edad = value; } }
+
+        public Person(int id, string nombre, int edad) 
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+
+        public void CompararEdad(int edadAComparar)
+        {
+            if (this.edad == edadAComparar)
+                Console.WriteLine($"Las edades son iguales.");
+            else
+                Console.WriteLine($"Las edades comparadas son distintas. {this.edad}, {edadAComparar}");
+        }
+    }
+}
---
+++ Persona.Semana5/Program.cs
@@ -0,0 +1,15 @@
+﻿using Persona.Semana5;
+    
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Person persona1 = new Person(1,"Simon", 21);
+        Person persona2 = new Person(2, "Juan", 22);
+        Person persona3 = new Person(3, "José", 22);
+        persona2.CompararEdad(persona1.Edad);
+        persona3.CompararEdad(persona2.Edad);
+    }
+}
+
+
