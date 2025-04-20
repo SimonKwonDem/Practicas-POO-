51955f11ebe5c3b89912afa0fa31089433010fe7
Simon Kwon <simonkwon03@gmail.com>
2025-04-20 16:00:41 -0300

    Ejercicio Ambientes

---
+++ Ambientes.Semana5/Ambiente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambientes.Semana5
+{
+    internal class Ambiente
+    {
+        private string sitema_Operativo;
+        private int ram;
+        private string base_Datos;
+        private string app;
+
+        public Ambiente(string sistema_Operativo, int ram, string base_Datos, string app) 
+        {
+            this.sitema_Operativo = sistema_Operativo;
+            this.ram = ram;
+            this.base_Datos = base_Datos;
+            this.app = app;
+        }
+        public void VerificarDespliegue(string sist_Op, int ram, string bdd, string app)
+        {
+            if (this.sitema_Operativo == sist_Op &&
+               this.ram == ram &&
+               this.base_Datos == bdd &&
+               this.app == app)
+                Console.WriteLine("Es posible realizar el despliegue. (;");
+            else 
+                Console.WriteLine("No es posible realizar el despliegue. ):");
+        }
+    }
+}
---
+++ Ambientes.Semana5/Program.cs
@@ -0,0 +1,26 @@
+﻿using Ambientes.Semana5;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Ambiente Desarrollo = new Ambiente("linux", 4, "postgresql", "openjdk");
+        Ambiente Testing = new Ambiente("windows", 4, "mysql", "dotnet");
+        Ambiente PreProducción = new Ambiente("linux", 8, "mysql", "openjdk");
+        Ambiente Producción = new Ambiente("linux", 4, "postgresql", "openjdk"); 
+
+        // Requerido para realizar el despliegue...
+        string sistOp = "linux";
+        int ram = 4;
+        string bDD = "postgresql";
+        string aPP = "openjdk"; 
+
+        Desarrollo.VerificarDespliegue(sistOp, ram, bDD, aPP);
+        Console.WriteLine("");
+        Testing.VerificarDespliegue(sistOp, ram, bDD, aPP);
+        Console.WriteLine("");
+        PreProducción.VerificarDespliegue(sistOp, ram, bDD, aPP);
+        Console.WriteLine("");
+        Producción.VerificarDespliegue(sistOp, ram, bDD, aPP);
+    }
+}
