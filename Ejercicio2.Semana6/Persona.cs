ea73233cdc2d6e88a63eee7dfc7b5f991de93f46
Simon Kwon <simonkwon03@gmail.com>
2025-04-24 20:07:59 -0300

    Ejercicio2

---
+++ Ejercicio2.Semana6/Persona.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2.Semana6
+{
+    internal class Persona
+    {
+        public string nombre;
+        private int edad;
+        public int dni;
+
+        public Persona()
+        {
+            Console.WriteLine("Constructor vacío.");
+        }
+        public Persona(string nombre, int edad)
+        {
+            Console.WriteLine("Constructor con dos parámetros");
+            this.nombre = nombre;
+            this.edad = edad;
+            dni = 0;
+        }
+        public Persona(string nombre, int edad, int dni)
+        {
+            Console.WriteLine("Constructor con tres parámetros");
+            this.nombre = nombre;
+            this.edad = edad;
+            this.dni = dni;
+        }
+        public void MostrarInfo()
+        {
+            Console.WriteLine($"Nombre: {nombre}");
+            Console.WriteLine($"Edad: {edad}");
+            Console.WriteLine($"DNI: {dni}");
+        }
+    }
+}
---
+++ Ejercicio2.Semana6/Program.cs
@@ -0,0 +1,16 @@
+﻿using Ejercicio2.Semana6;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Persona p1 = new Persona();
+        p1.MostrarInfo();
+        Console.WriteLine("");
+        Persona p2 = new Persona("Simon", 21);
+        p2.MostrarInfo();
+        Console.WriteLine("");
+        Persona p3 = new Persona("Juan", 22, 33111222);
+        p3.MostrarInfo();
+    }
+}
